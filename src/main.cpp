f175661851aceb31b378c60d163dfcfd4636468c
CurentUser <curent_user@mail.ru>
2021-10-28 21:48:01 +0300

    added glfw and glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,63 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
-#include <GLFW3/glwf3.h>
 
-int main(void)
-{
-	std::cout << "EdgaEngine2D: is game 2D engine, developer CurrentUser.\n";
-    GLFWwindow* window;
+void WindowResize(GLFWwindow* window, int width, int height);
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+int main() {
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+// main window setings
+GLFWwindow* window = nullptr;
+unsigned int window_width = 800;
+unsigned int window_height = 600;
+const char* window_name = "EdgaEngine2D";
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        //glClear(GL_COLOR_BUFFER_BIT);
+const char** glfw_error = nullptr;
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+  if ( !glfwInit() ) { // initialization glfw
+	glfwGetError(glfw_error);
+	std::cout << "Can`t initialization GLFW, error:" << glfw_error << "\n";
+	return -1;
+  }
+  
+  // seting opengl contexts
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+  // creating main window
+  window = glfwCreateWindow(window_width, window_height, window_name, NULL, NULL);
+  if (!window) {
+	glfwGetError(glfw_error);
+	std::cout << "Can`t initialization window, error:" << glfw_error << "\n";
+	glfwTerminate();
+	return -1;
+  }
+  glfwMakeContextCurrent(window);
 
-    glfwTerminate();
-    return 0;
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) { // initialization glad
+	  std::cout << "Cat`t initialization GLAD \n";
+	  return -1;
+  }
+
+  glViewport(0, 0, window_width, window_height); // domain of viewer
+  glfwSetFramebufferSizeCallback(window,WindowResize); // callback for resize window
+  
+  while (!glfwWindowShouldClose(window)) { // loop of render
+
+
+	  glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+	  glClear(GL_COLOR_BUFFER_BIT);
+
+	  glfwSwapBuffers(window);
+	  glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+}
+
+void WindowResize(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
 }
